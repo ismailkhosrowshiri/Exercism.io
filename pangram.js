4731902a7c106e6de55ea4886a0a6eb77322290e
ismailkhosrowshiri <80094128+ismailkhosrowshiri@users.noreply.github.com>
2021-07-16 21:23:05 +0430

    Add files via upload

---
+++ pangram.js
@@ -0,0 +1,16 @@
+
+export const isPangram = (stringg) => {
+  const string = stringg.toLowerCase();
+  if (string==="")
+  return false
+
+  const alphabet= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  let pangram = true;
+  for (let i = 0; i < alphabet.length; i++) {
+    if (string.includes(alphabet[i]) === false)
+      pangram = false
+  }
+  return pangram
+
+
+};
